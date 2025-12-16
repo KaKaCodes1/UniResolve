3e340dcdc08347ea3953f7ff757e026da3aca4c8
Hamisi_Juma <hamisig123@gmail.com>
2025-12-16 21:02:30 +0300

    Creating the UserManager

--- Uniresolve/accounts/models.py
+++ Uniresolve/accounts/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+#User Manager
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        
+        # Normalize the email for consistency ie lowercase the domain name 
+        email = self.normalize_email(email)
+
+        #creating a user instance
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
 
 # User Model
 class User(AbstractUser):
@@ -22,8 +41,10 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [ 'first_name', 'last_name']
 
+    objects = UserManager() #connecting the manager with the User model
+
     def __str__(self):
-        return self.get_full_name()
+        return f"{self.get_full_name()} - {self.role}"
 
     
 
