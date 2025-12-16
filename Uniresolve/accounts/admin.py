211f19b6a3a0f23fb181c2f58dc5a51035a29108
Hamisi_Juma <hamisig123@gmail.com>
2025-12-16 22:19:16 +0300

    register accounts models in the admin.py

--- Uniresolve/accounts/admin.py
+++ Uniresolve/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, StudentProfile, StaffProfile
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(StaffProfile)
+admin.site.register(StudentProfile)
