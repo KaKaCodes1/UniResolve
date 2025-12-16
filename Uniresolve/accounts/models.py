0bdaea210514c5e7518abfa7b57f0131ca9ec4e0
Hamisi_Juma <hamisig123@gmail.com>
2025-12-16 20:43:08 +0300

    Created the user model

--- Uniresolve/accounts/models.py
+++ Uniresolve/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+# User Model
+class User(AbstractUser):
+    roles_choices = [
+        ('Student', 'Student'),
+        ('Staff', 'Staff'),
+        ('Admin','Admin'),
+    ]
+
+    role = models.CharField(max_length=50, choices=roles_choices, default='Student')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    
+    #Keeping username optional
+    username = models.CharField(max_length=100, unique=True, null=True, blank=True)
+
+    # Use email as the login field instead of username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [ 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.get_full_name()
+
+    
+
+
+
