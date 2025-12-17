c46cb05ace348809ada80a99b55d0ffb90f34fde
Hamisi_Juma <hamisig123@gmail.com>
2025-12-17 14:58:14 +0300

    registered the models of organization and tickets app in their respective admin.py

--- Uniresolve/tickets/admin.py
+++ Uniresolve/tickets/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Resolution, Ticket
 # Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category')
+    search_fields = ('title', 'description', 'owner__email')
+
+@admin.register(Resolution)
+class ResolutionAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'resolved_by', 'resolved_at')
+
+# admin.site.register(Ticket)
+# admin.site.register(Resolution)
+# admin.site.register(TicketAdmin)
+# admin.site.register(ResolutionAdmin)
