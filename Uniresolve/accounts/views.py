d23756389f2d4648789d7dba487d8d7895968109
Hamisi_Juma <hamisig123@gmail.com>
2025-12-18 12:07:42 +0300

    Worked on UserRegistrationView and the urls responsible

--- Uniresolve/accounts/views.py
+++ Uniresolve/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny] #allow access to this view to anyone as it is the registration view
