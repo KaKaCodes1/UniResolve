7e6d4bbc35d6f8aa332541b1828a2aa9b9732f34
Hamisi_Juma <hamisig123@gmail.com>
2025-12-19 20:09:53 +0300

    Add filters for viewing tickets per student and department(for staff)

--- Uniresolve/tickets/views.py
+++ Uniresolve/tickets/views.py
@@ -5,9 +5,23 @@ from .models import Ticket, Resolution
 
 # Create your views here.
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
+    # queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes =[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        #Students only see tickets they created
+        if user.role == 'Student':
+            return Ticket.objects.filter(owner = user)
+        
+        if user.role == 'Staff':
+            staff_dept = user.staff_profile.department
+            return Ticket.objects.filter(category__department = staff_dept)
+        
+        #Admins see all tickets
+        return Ticket.objects.all()
 
     #connects a ticket to a logged in user
     def perform_create(self, serializer):
