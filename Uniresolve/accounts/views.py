e61f4a27aab3f4352ab9780d340e165bd0690ce8
Hamisi_Juma <hamisig123@gmail.com>
2026-01-19 14:56:17 +0300

    TemplateViews for sign up pages

--- Uniresolve/accounts/views.py
+++ Uniresolve/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializers import UserRegistrationSerializer
 from django.contrib.auth import get_user_model
+from django.views.generic import TemplateView 
+from organization.models import Course, Department 
 
 User = get_user_model()
 
@@ -11,19 +13,24 @@ class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny] #allow access to this view to anyone as it is the registration view
 
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from .forms import StudentSignUpForm, StaffSignUpForm
 
-class StudentSignUpView(CreateView):
-    model = User
-    form_class = StudentSignUpForm
+# View for the Student Sign-Up Page
+# Renders the HTML template and provides the list of Courses for the dropdown
+class StudentSignUpPageView(TemplateView):
     template_name = 'accounts/signup_student.html'
-    success_url = reverse_lazy('login') # TODO: Ensure this points to a template-based login view later
 
-class StaffSignUpView(CreateView):
-    model = User
-    form_class = StaffSignUpForm
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.all() # Pass all courses to the template
+        return context
+
+# View for the Staff Sign-Up Page
+# Renders the HTML template and provides the list of Departments for the dropdown
+class StaffSignUpPageView(TemplateView):
     template_name = 'accounts/signup_staff.html'
-    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['departments'] = Department.objects.all() # Pass all departments to the template
+        return context
 
