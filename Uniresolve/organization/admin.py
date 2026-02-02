7e337cc06b6b1005f28824be821e4d807d630778
Hamisi_Juma <hamisig123@gmail.com>
2026-02-02 23:56:43 +0300

    edited the organization's admin.py

--- Uniresolve/organization/admin.py
+++ Uniresolve/organization/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Department, Category, Course
 
 # Register your models here.
-admin.site.register(Department)
-admin.site.register(Category)
-admin.site.register(Course)
+# admin.site.register(Department)
+# admin.site.register(Category)
+# admin.site.register(Course)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('department_name',)
+    list_filter = ('department_name',)
+    search_fields = ('department_name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'department')
+    list_filter = ('department',)
+    search_fields = ('category_name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_name', 'department')
+    list_filter = ('department',)
+    search_fields = ('course_name',)
+
